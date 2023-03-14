e4cea2c970d5cb6c59512f498f77e90582766a76
James Wrigley <james.wrigley@xfel.eu>
2023-03-14 16:12:20 +0100

    Improve benchmarks and loader

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -14,7 +14,13 @@ function mmap_read(h5_path, ds_name)
     mapped_file = mmap(h5_path)
 
     ds = f[ds_name]
-    addrs, sizes = XFA.getchunkaddrs(ds)
+    addrs, sizes = h5open(XFA.indexpath(h5_path)) do index_file
+        addrs = HDF5.readmmap(index_file["$(ds_name)/addrs"])
+        sizes = HDF5.readmmap(index_file["$(ds_name)/sizes"])
+
+        return addrs, sizes
+    end
+
     XFA.loadchunks(ds, mapped_file, addrs, sizes)
     close(f)
 end
@@ -25,48 +31,80 @@ function default_read(h5_path, ds_name)
     end
 end
 
-function mkbenchmarks(data_dir)
-    h5_path = joinpath(data_dir, H5_NAME)
+function dataset_typename(h5_path, ds_name)
+    h5open(h5_path) do f
+        ds = f[ds_name]
+        dtype = lowercase(string(eltype(ds)))
 
+        return (XFA.iscompressed(ds) ? "compressed_" : "") * dtype
+    end
+end
+
+function mkbenchmarks(h5_path; generated_file=false)
     suite = BenchmarkGroup()
     suite["hdf5_reader"] = BenchmarkGroup()
     suite["hdf5_reader"]["mmap"] = BenchmarkGroup()
     suite["hdf5_reader"]["default"] = BenchmarkGroup()
 
-    for ds_name in ["float32", "compressed_uint16"]
-        suite["hdf5_reader"]["mmap"][ds_name] = @benchmarkable mmap_read($h5_path, $ds_name) samples=10
-        suite["hdf5_reader"]["default"][ds_name] = @benchmarkable default_read($h5_path, $ds_name) samples=10
+    file_datasets = if generated_file
+        ["float32", "compressed_uint16"]
+    else
+        ["INSTRUMENT/MID_DET_AGIPD1M-1/DET/0CH0:xtdf/image/data",
+         "INSTRUMENT/MID_DET_AGIPD1M-1/DET/0CH0:xtdf/image/mask"]
+    end
+    ds_names = Dict([dataset_typename(h5_path, x) => x for x in file_datasets])
+
+    # Create indexes if they don't already exist
+    if !XFA.hasindex(h5_path)
+        print("Creating indexes...")
+        XFA.writeindex(h5_path, file_datasets)
+        println(" done")
+    end
+
+    for (k, v) in ds_names
+        suite["hdf5_reader"]["mmap"][k] = @benchmarkable mmap_read($h5_path, $v) samples=10
+        suite["hdf5_reader"]["default"][k] = @benchmarkable default_read($h5_path, $v) samples=10
     end
 
     return suite
 end
 
-function runbenchmarks(n_trains::Int=10)
-    # Write dummy data
-    data_dir = mktempdir(pwd())
-    h5open(joinpath(data_dir, H5_NAME), "w") do f
-        float32_data = rand(Float32, 128, 512, 352 * n_trains)
-        uint16_data = rand(UInt16, size(float32_data))
+function runbenchmarks(n_trains::Int=10, data_dir=pwd(); use_file=nothing)
+    # Write dummy data if necessary
+    generated_file = use_file === nothing
+    if generated_file
+        data_dir = mktempdir(data_dir)
+        h5open(joinpath(data_dir, H5_NAME), "w") do f
+            float32_data = rand(Float32, 128, 512, 352 * n_trains)
+            uint16_data = rand(UInt16, size(float32_data))
 
-        f["float32", chunk=(128, 512, 1)] = float32_data
-        f["compressed_uint16", chunk=(128, 512, 1), shuffle=(), deflate=1] = uint16_data
+            f["float32", chunk=(128, 512, 1)] = float32_data
+            f["compressed_uint16", chunk=(128, 512, 1), shuffle=true, deflate=1] = uint16_data
+        end
     end
 
-    h5_size = filesize(joinpath(data_dir, H5_NAME))
-    println(@sprintf "Wrote %d trains into a %.2fMB file\n" n_trains (h5_size / 1e6))
+    h5_path = if generated_file
+        joinpath(data_dir, H5_NAME)
+    else
+        use_file
+    end
+
+    h5_size = filesize(h5_path)
+    println(@sprintf "Benchmarking a %.2fMB file\n" (h5_size / 1e6))
 
     results = nothing
     try
-        suite = mkbenchmarks(data_dir)
+        suite = mkbenchmarks(h5_path; generated_file=generated_file)
         results = run(suite; verbose=true)
-    catch e
-        throw(e)
     finally
-        rm(data_dir; recursive=true)
+        if generated_file
+            rm(data_dir; recursive=true)
+        end
     end
 
     println()
-    for ds_name in ["float32", "compressed_uint16"]
+    group = results["hdf5_reader"]["mmap"]
+    for ds_name in keys(group)
         mmap_results = results["hdf5_reader"]["mmap"][ds_name]
         default_results = results["hdf5_reader"]["default"][ds_name]
 
@@ -77,4 +115,3 @@ function runbenchmarks(n_trains::Int=10)
 
     return results
 end
-
--- src/run_streamer.jl
+++ src/run_streamer.jl
@@ -1,3 +1,5 @@
+using Distributed
+
 using HDF5
 using Hwloc
 using LibDeflate
@@ -48,14 +50,74 @@ function getchunkaddrs(ds::HDF5.Dataset)
     return (addrs, sizes)
 end
 
-function deshuffle(data, out, eltype_size=4)
-    byte_stride = length(out) ÷ eltype_size
+function getchunkaddrs(path_and_ds::Tuple{String, String})
+    h5_path, ds_name = path_and_ds
 
-    @simd for i in 1:byte_stride
-        out_i = 1 + (i - 1) * 2 # eltype_size
-
-        Base.Cartesian.@nexprs 2 j -> @inbounds out[out_i + j - 1] = data[i + ((j - 1) * byte_stride)]
+    h5open(h5_path) do f
+        getchunkaddrs(f[ds_name])
     end
+end
+
+function indexpath(h5_path)
+    h5_path = abspath(h5_path)
+    data_dir = splitpath(h5_path)[end - 1]
+
+    return "$(splitext(h5_path)[1])-index.h5"
+end
+
+hasindex(h5_path) = isfile(indexpath(h5_path))
+
+function writeindex(h5_path, ds_names)
+    index_path = indexpath(h5_path)
+
+    readers = addprocs(length(ds_names))
+    Distributed.remotecall_eval(Main, readers, :(using XFA))
+
+    try
+        h5open(index_path, "w") do index_file
+            indexes = pmap(getchunkaddrs, [(h5_path, x) for x in ds_names])
+
+            for (ds_name, (addrs, sizes)) in Iterators.zip(ds_names, indexes)
+                index_file["$(ds_name)/addrs"] = addrs
+                index_file["$(ds_name)/sizes"] = sizes
+            end
+        end
+    finally
+        rmprocs(readers)
+    end
+
+    return index_path
+end
+
+function deshuffle_impl(data, out::Type{A}) where A <: AbstractArray{T} where T
+    eltype_size = sizeof(T)
+
+    quote
+        byte_stride = length(data) ÷ $eltype_size
+        GC.@preserve out begin
+            out_ptr = Base.unsafe_convert(Ptr{UInt8}, out)
+            out_bytes = unsafe_wrap(Array, out_ptr, sizeof(out))
+
+            @inbounds for i in 1:byte_stride
+                out_i = 1 + (i - 1) * $eltype_size
+
+                Base.Cartesian.@nexprs $eltype_size j -> out_bytes[out_i + j - 1] = data[i + ((j - 1) * byte_stride)]
+            end
+        end
+    end
+end
+
+@generated function deshuffle(data::Vector{UInt8}, out)
+    # 1 + 1
+    deshuffle_impl(data, out)
+end
+
+function iscompressed(ds::HDF5.Dataset)
+    ds_properties = HDF5.get_create_properties(ds)
+    filters = ds_properties.filters
+    filter_types = typeof.(filters)
+
+    return Filters.Deflate in filter_types
 end
 
 function loadchunks(ds::HDF5.Dataset, mapped_file::Vector{UInt8},
@@ -135,12 +197,13 @@ function loadchunks(ds::HDF5.Dataset, mapped_file::Vector{UInt8},
                                     compressed_chunk = @view mapped_file[c_offset:c_offset + c_size - 1]
                                     final_dest_addr = 1 + (global_chunk_idx - 1) * chunk_size
                                     final_dest = @view dest[final_dest_addr:final_dest_addr + chunk_size - 1]
+                                    final_dest_typed = reinterpret(eltype(ds), final_dest)
 
                                     unsafe_zlib_decompress!(Base.HasLength(), codec,
                                                             pointer(decompression_tmp), length(decompression_tmp),
                                                             pointer(compressed_chunk), length(compressed_chunk))
 
-                                    deshuffle(decompression_tmp, final_dest, eltype_size)
+                                    deshuffle(decompression_tmp, final_dest_typed)
 
                                     c_offset += sizes[global_chunk_idx]
                                 end
@@ -155,6 +218,7 @@ function loadchunks(ds::HDF5.Dataset, mapped_file::Vector{UInt8},
                         end
                     end
                     for (thread_id, group) in enumerate(chunk_groups)]
+
     fetcher = Threads.@spawn begin
         try
             foreach(fetch, reader_tasks)
